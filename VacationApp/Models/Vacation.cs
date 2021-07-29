26de93f77aa2cb98d19ae31e35738221528f2000
Patrashkin Egor <patrashkin@labsys.ru>
2021-07-29 16:50:54 +0500

    Vacation model begin

---
+++ VacationApp/Models/Vacation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationApp.Models
+{
+    public class Vacation
+    {
+        public Employee Employee{ get; set; }
+        public DateTime Begin { get; set; }
+        public DateTime End { get; set; }
+    }
+}
