bbb9084fd33a315e7ae5f8994ba3a41cead7ddd6
Patrashkin Egor <patrashkin@labsys.ru>
2021-07-29 16:49:37 +0500

    Add Female surnames test

---
+++ TestVacationApp/Services/RandomEmployeeGeneratorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using VacationApp.Serviсes;
+
+namespace TestVacationApp.Services
+{
+    public class RandomEmployeeGeneratorTest
+    {
+        [Theory]
+        [InlineData("Сидоров", "Сидорова")]
+        [InlineData("Королёв", "Королёва")]
+        [InlineData("Бородин", "Бородина")]
+        [InlineData("Короткий", "Короткая")]
+        [InlineData("Проклятый", "Проклятая")]
+        [InlineData("Мостовой", "Мостовая")]
+        [InlineData("Шеппард", "Шеппард")]
+        public void FemaleSurnameTest(string MaleSurname, string ExpectedFemaleSurname)
+        {
+            string FemaleSurname = RandomEmployeeGenerator.createFemaleSurnameFromMale(MaleSurname);
+            Assert.Equal(ExpectedFemaleSurname, FemaleSurname);
+        }
+    }
+}
