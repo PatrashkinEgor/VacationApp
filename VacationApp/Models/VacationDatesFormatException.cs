95ef130f76fb8ebcf4790277df103065be870923
Егор Патрашкин <eapat@mail.ru>
2021-07-31 16:27:58 +0500

    Add VacationDatesFormatException

---
+++ VacationApp/Models/VacationDatesFormatException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationApp.Models
+{
+    class VacationDatesFormatException : FormatException
+    {
+        public VacationDatesFormatException(string message)
+            : base(message)
+        { }
+    }
+}
